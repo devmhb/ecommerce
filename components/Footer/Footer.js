9ce9875f72326d07e5f092dc2fa41eb81c78a3a7
hrshainik <hrsshainik@gmail.com>
2021-05-28 01:18:20 +0600

    home page basic structure done

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,8 +1,116 @@
 import React from "react";
 import styles from "./footer.module.scss";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.col}>
+        <h4 className={styles.title}>Get in touch</h4>
+        <ul className={styles.items}>
+          <li className={styles.item}>
+            <Link href="">
+              <a>About</a>
+            </Link>
+          </li>
+          <li className={styles.item}>
+            <Link href="">
+              <a>Careers</a>
+            </Link>
+          </li>
+          <li className={styles.item}>
+            <Link href="">
+              <a>Press Releases</a>
+            </Link>
+          </li>
+          <li className={styles.item}>
+            <Link href="">
+              <a>Blog</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      <div className={styles.col}>
+        <h4 className={styles.title}>Get in touch</h4>
+        <ul className={styles.items}>
+          <li className={styles.item}>
+            <Link href="">
+              <a>About</a>
+            </Link>
+          </li>
+          <li className={styles.item}>
+            <Link href="">
+              <a>Careers</a>
+            </Link>
+          </li>
+          <li className={styles.item}>
+            <Link href="">
+              <a>Press Releases</a>
+            </Link>
+          </li>
+          <li className={styles.item}>
+            <Link href="">
+              <a>Blog</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      <div className={styles.col}>
+        <h4 className={styles.title}>Get in touch</h4>
+        <ul className={styles.items}>
+          <li className={styles.item}>
+            <Link href="">
+              <a>About</a>
+            </Link>
+          </li>
+          <li className={styles.item}>
+            <Link href="">
+              <a>Careers</a>
+            </Link>
+          </li>
+          <li className={styles.item}>
+            <Link href="">
+              <a>Press Releases</a>
+            </Link>
+          </li>
+          <li className={styles.item}>
+            <Link href="">
+              <a>Blog</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      <div className={styles.col}>
+        <h4 className={styles.title}>Get in touch</h4>
+        <ul className={styles.items}>
+          <li className={styles.item}>
+            <Link href="">
+              <a>About</a>
+            </Link>
+          </li>
+          <li className={styles.item}>
+            <Link href="">
+              <a>Careers</a>
+            </Link>
+          </li>
+          <li className={styles.item}>
+            <Link href="">
+              <a>Press Releases</a>
+            </Link>
+          </li>
+          <li className={styles.item}>
+            <Link href="">
+              <a>Blog</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <p className={styles.copyright}>Copyright © 2020 Ecommerce</p>
+    </footer>
+  );
 };
 
 export default Footer;
