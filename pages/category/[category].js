2d62041605b9b95a3a1321e29d77e92bfadd2ae7
hrshainik <hrsshainik@gmail.com>
2021-05-30 19:08:13 +0600

    add products.json from strapi

--- pages/category/[category].js
+++ pages/category/[category].js
@@ -1,31 +1,35 @@
-import React from 'react';
-import { useRouter } from "next/router"
+import React from "react";
+import { useRouter } from "next/router";
 import Head from "next/head";
-import styles from "./category.module.scss"
-import ProductsContainer from '../../components/ProductsContainer/ProductsContainer';
-import Footer from '../../components/Footer/Footer';
-import NavBar from '../../components/NavBar/NavBar'
+import styles from "./category.module.scss";
+import ProductsContainer from "../../components/ProductsContainer/ProductsContainer";
+import Footer from "../../components/Footer/Footer";
+import NavBar from "../../components/NavBar/NavBar";
 
 const Category = () => {
-    const router = useRouter();
-    const { category } = router.query;
+  const router = useRouter();
+  const { category } = router.query;
 
-    return (
-        <>
-        <Head>
-            <title>{category}</title>
-        </Head>
-        <div className={styles.category}>
-            <NavBar />
-            <header className={styles.header}>
-                <h1>Category</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate adipisci dolorem itaque corrupti aperiam accusamus dicta ex doloremque iste quam.</p>
-            </header>
-            <ProductsContainer />
-            <Footer />
-        </div>
-        </>
-    )
-}
+  return (
+    <>
+      <Head>
+        <title>{category}</title>
+      </Head>
+      <div className={styles.category}>
+        <NavBar />
+        <header className={styles.header}>
+          <h1>{category}</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate
+            adipisci dolorem itaque corrupti aperiam accusamus dicta ex
+            doloremque iste quam.
+          </p>
+        </header>
+        <ProductsContainer />
+        <Footer />
+      </div>
+    </>
+  );
+};
 
-export default Category
+export default Category;
