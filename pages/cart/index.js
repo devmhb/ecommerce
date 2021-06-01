8976ff120febad7675e9417c4d5954e6f27953de
hrshainik <hrsshainik@gmail.com>
2021-06-02 01:50:56 +0600

    Remove unused navbar component

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import NavBar from "../../components/NavBar/NavBar";
 import OrderedProduct from "../../components/OrderedProduct/OrderedProduct";
 import styles from "../../styles/Cart.module.scss";
 import Head from "next/head";
